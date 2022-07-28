e7b9f2ae6da69ba8a74ef01a0158e1a7dc8e5c6d
katacoder48 <katacoder48@gmail.com>
2022-07-28 14:52:47 +0900

    add LICENSE & MANIFEST & setup.py

--- setup.py
+++ setup.py
@@ -5,4 +5,10 @@ setup(
     name='passwordTx',
     version='0.1',
     packages=['passwordTx'],
+    url="https://github.com/theKata/passwordTx",
+    python_requires='>=3.6',
+    install_requires=[
+        "cryptography>=37.0.0",
+        "web3>=5.29.0"
+    ]
 )
